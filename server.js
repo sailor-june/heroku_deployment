4660f6767f576779dcf013f0f10dff579325dc41
Juniper James <vvlpespro@gmail.com>
2022-12-20 12:18:33 -0500

    it should work

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = mongoose.connection;
 //Port
 //___________________
 // Allow use of Heroku's port or your own local port, depending on the environment
-
+const PORT = process.env.PORT || 3000;
 
 //___________________
 //Database
